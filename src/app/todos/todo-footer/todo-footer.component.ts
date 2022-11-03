4d1e25845158eed836bbccb5ee53b757487a4487
Esperanza Najera <esperanza.nave@gmail.com>
2022-11-03 10:57:11 -0700

    count taks

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -12,12 +12,17 @@ export class TodoFooterComponent implements OnInit {
   filtroActual: filtrosValidos = 'todos';
   filtros: filtrosValidos[] = ['todos', 'completados', 'pendientes'];
 
+  pendientes: number = 0;
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
-    this.store.select('filtro').subscribe(filtro =>{
+   /*  this.store.select('filtro').subscribe(filtro =>{
      this.filtroActual = filtro;
-    })
+    }) */
+    this.store.subscribe(state =>{
+       this.filtroActual= state.filtro;
+       this.pendientes = state.todos.filter(todo => !todo.completado).length;
+    });
   }
 
   cambiarFiltro (filtro: filtrosValidos)
