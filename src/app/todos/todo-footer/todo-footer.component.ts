64046dd76363e9b0cca522f26a33b87c0b8ce495
Esperanza Najera <esperanza.nave@gmail.com>
2022-11-02 22:58:45 -0700

    change filter

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { filtrosValidos, setFiltro } from 'src/app/filtro/filtro.actions';
 
 @Component({
   selector: 'app-todo-footer',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-footer.component.css']
 })
 export class TodoFooterComponent implements OnInit {
+  filtroActual: filtrosValidos = 'todos';
+  filtros: filtrosValidos[] = ['todos', 'completados', 'pendientes'];
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.store.select('filtro').subscribe(filtro =>{
+     this.filtroActual = filtro;
+    })
   }
 
+  cambiarFiltro (filtro: filtrosValidos)
+{
+   this.store.dispatch(setFiltro({filtro}))
+}
 }
